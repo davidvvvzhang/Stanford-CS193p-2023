0311c27eacb5a993d3146a5181f9a4dcd20e001a
davidvvvzhang <2801442102@qq.com>
2025-03-04 18:13:48 +0800

    Create MemorizeApp.swift

---
+++ MemorizeApp.swift
@@ -0,0 +1,19 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by David on 2025/1/28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    @StateObject var game = EmojiMemoryGame()
+    
+    var body: some Scene {
+        WindowGroup {
+            EmojiMemoryGameView(viewModel: game)
+        }
+    }
+}
