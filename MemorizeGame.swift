47b06b575d3c8bf63dbc6d29461cadc635238b6e
davidvvvzhang <2801442102@qq.com>
2025-03-04 18:14:18 +0800

    Create MemorizeGame.swift

---
+++ MemorizeGame.swift
@@ -0,0 +1,37 @@
+//
+//  MemorizeGame.swift
+//  Memorize
+//
+//  Created by David on 2025/2/28.
+//
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    private(set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+        print(cards)
+    }
+    
+    struct Card {
+        var isFaceUp = true
+        var isMatched = false
+        let content: CardContent
+    }
+}
