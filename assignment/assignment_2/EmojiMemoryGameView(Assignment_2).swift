2541d1503a009afc15b4ca11306d8129a178444f
davidvvvzhang <2801442102@qq.com>
2025-03-07 13:57:20 +0800

    Create EmojiMemoryGameView(Assignment_2).swift

---
+++ assignment/assignment_2/EmojiMemoryGameView(Assignment_2).swift
@@ -0,0 +1,82 @@
+//
+//  EmojiMemoryGameView(Assignment_2).swift
+//  Memorize
+//
+//  Created by David on 2025/1/28.
+//
+
+import SwiftUI
+
+struct EmojiMemoryGameView: View {
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    var body: some View {
+        VStack {
+            Text(viewModel.themeName)
+                .font(.largeTitle)
+                .bold()
+                .padding()
+            Text("Score: \(viewModel.score)")
+                .font(.title)
+                .bold()
+                .padding(.bottom, 10)
+            ScrollView {
+                cards
+                    .animation(.default, value: viewModel.cards)
+            }
+            Button("Shuffle") {
+                viewModel.shuffle()
+            }
+            Button("New Game") {
+                viewModel.newGame()
+            }
+        }
+        .padding()
+        .foregroundColor(viewModel.themeColor)
+    }
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
+            ForEach(viewModel.cards) { card in
+                CardView(card)
+                    .aspectRatio(2/3, contentMode: .fit)
+                    .padding(4)
+                    .onTapGesture {
+                        viewModel.choose(card)
+                    }
+            }
+        }
+        .foregroundColor(viewModel.themeColor)
+    }
+}
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base: RoundedRectangle = RoundedRectangle(cornerRadius: 12)
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+                .opacity(card.isFaceUp ? 1 : 0)
+            base.fill()
+                .opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+        .animation(.default, value: card.isFaceUp)
+    }
+}
+
+#Preview {
+    EmojiMemoryGameView(viewModel: EmojiMemoryGame())
+}
