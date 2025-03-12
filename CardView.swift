8eb01cff4e275cec5ac57b2e8fbb82b64e9db9c5
davidvvvzhang <2801442102@qq.com>
2025-03-12 15:39:10 +0800

    Create CardView.swift

---
+++ CardView.swift
@@ -0,0 +1,53 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by David on 3/11/25.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+    
+    let card: Card
+    
+    init(_ card: Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        Pie(endAngle: .degrees(240))
+            .opacity(Constants.Pie.opacity)
+            .overlay(
+                Text(card.content)
+                    .font(.system(size: Constants.FontSize.largest))
+                    .minimumScaleFactor(Constants.FontSize.scaleFactor)
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(Constants.Pie.inset)
+            )
+            .padding(Constants.inset)
+            .cardify(isFaceUp: card.isFaceUp)
+            .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+    
+    private struct Constants {
+        static let inset: CGFloat = 5
+        struct FontSize {
+            static let largest: CGFloat = 200
+            static let smallest: CGFloat = 10
+            static let scaleFactor = smallest / largest
+        }
+        struct Pie {
+            static let opacity: CGFloat = 0.5
+            static let inset: CGFloat = 5
+        }
+    }
+}
+
+#Preview {
+    CardView(MemoryGame<String>.Card(content: "X", id: "test1"))
+        .padding()
+        .foregroundColor(.green)
+}
